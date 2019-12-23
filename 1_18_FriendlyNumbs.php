05482c2deacdfe75f4eab80ebdf9be20d0f39216
dekantis <dekantis@gmail.com>
2019-12-23 14:13:04 +0300

    Задача решена (Но не уверен, что правильно)

---
+++ 1_18_FriendlyNumbs.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Два  натуральных  числа  называют  дружественными,
+если  каждое  из них равно сумме всех делителей другого.
+Найти все пары дружественных чисел, лежащих в диапазоне от N до M.
+*/
+
+//Функция проверки на дружественные числа
+function sumOfDividers(int $numb): int
+{
+    $sum = 1;
+    for ($i=2;$i<=$numb/2;$i++) {//итерации до N/2
+        if ($numb % $i == 0) {
+            $sum += $i;
+        }
+    }
+    return $sum;
+}
+
+$N = 1;
+$M = 10000;
+for ($i=$N; $i<=$M; $i++) {
+    if (sumOfDividers(sumOfDividers($i)) == $i && sumOfDividers($i)!=$i) {
+        echo $i." ".sumOfDividers($i). "\n";
+        $i = sumOfDividers($i);
+    }
+}
+

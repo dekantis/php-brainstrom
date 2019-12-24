95951999c4efc0347376363bfbc13122dc0a7622
dekantis <dekantis@gmail.com>
2019-12-24 13:05:36 +0300

    Задача решена

---
+++ 1_22_primeFactors.php
@@ -0,0 +1,25 @@
+<?php
+
+/*Натуральное число N разложить на простые множители.*/
+
+//Функция поиска простых множителей
+function primeFactor(int $numb): int
+{
+    if ($numb == 1) {
+        return 1;
+    }
+    for ($i=2; $i<=$numb/2; $i++){
+        if ($numb % $i == 0) {
+            return $i;
+        }
+    }
+    return $numb;
+}
+$N = 22; //Натуральное число
+$str = "$N : "; //Строка для наглядности вывода разложенного числа
+
+while ($N>1) {
+    $str .= primeFactor($N)." * ";
+    $N /= primeFactor($N);
+}
+echo $str."1";

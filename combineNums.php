c4c7e8df736d1c5661b0168bde1b2d2eed924fa9
dekantis <dekantis@gmail.com>
2020-01-10 11:42:37 +0300

    Реализация алгоритма 1) Создание экземпляра класса 2) Проверка входящих переменных - - -> вывод ошибок - - -> завершение 3) Заполнение массива комбинациями 4) Вывод массива 5) Вывод значения формулы

--- combineNums.php
+++ combineNums.php
@@ -1,18 +1,65 @@
 <?php
 
+/*
+Надо создать класс, который
+будет выводить все возможные комбинации без повторений из строки символов, установленной длинны.
+Так же кол-во комбинаций.
+Например, вводим строку "123", и длинну строки 2, получаем
+12 13 23 21 32 31
+Если строка будет "111", то по логике размещений всё равно должно получиться
+11 11 11 11 11
++ нужен метод который считает размещения, в обход основного алгоритма.
+Это не сложная формула из комбинаторики  - размещения без повторений.
+n!/(n - m)!     n - размер множества      m - размер искомого подмножества
+По ней мы будем сверять результат работы.
+
+Должна быть обработка ошибок.
+метод записи ошибки
+метод получения ошибок
+метод отображения ошибки
+
+Класс должен работать со своим экземпляром ($this), это озночает, что не
+не должно быть статических методов.
+*/
+
 
 class CombineNums
 {
     private $arr = [];
-    private $countCombinations;
-    public function __construct(string $str, int $len)
+    private $errors = [];
+
+    private function setErrors ($str, $len)
     {
-        $this->combine($str, $len);
-        $this->countCombinations = $this->calculateCombinations($str, $len);
+        if (empty($str) || empty($len)) {
+            $this->errors[] = "Не введена строка \$str или длинна \$len подстроки";
+        }
+        if (gettype($str) != "string" || strlen($str) < 1) {
+            $this->errors[] = "Неверно введенная строка \$str";
+        }
+        if (gettype($len) != "integer" || $len < 1) {
+            $this->errors[] = "Неверно задана длинна подстрок \$len";
+        }
+        if (strlen($str) < $len) {
+            $this->errors[] = "Количство символов в строке \$str не должно быть меньше длинны \$len подстроки";
+        }
     }
 
+    public function showErrors()
+    {
+        foreach ($this->errors as $error) {
+            echo "$error \n";
+        }
+    }
 
-    private function combine(string $str, int $len, $resultStr = "")
+    public function getErrors($str, $len) {
+        $this->setErrors($str, $len);
+        if ($this->errors) {
+            return true;
+        }
+        return false;
+    }
+
+    public function combine(string $str, int $len, $resultStr = "")
     {
         for ($i = 0; $i < strlen($str); $i++) {
             $resultStr .= $str[$i];
@@ -31,7 +78,7 @@ class CombineNums
         return $number > 1 ? $number * $this->factorial($number-1) : 1;
     }
 
-    private function calculateCombinations(string $str, int $len) : int
+    public function getCalculateCombinations(string $str, int $len) : int
     {
         return $this->factorial(strlen($str)) / ($this->factorial(strlen($str)-$len));
     }
@@ -40,30 +87,19 @@ class CombineNums
     {
         return $this->arr;
     }
-
-    public function getCountCombinations () : int
-    {
-        return $this->countCombinations;
-    }
 }
+//Входные параметры (строка и длинна подстрок вхождений)
+$str = "asdfgh";
+$len = 5;
 
-try {
-    $str = "str";
-    $len = 2;
-    if (empty($str) || gettype($str) != "string") {
-        throw new Exception("пустая или неверно введенная строка \$str");
-    }
-    if (empty($len) || gettype($len) != "integer") {
-        throw new Exception("пустая или неверно введенная переменная \$len");
-    }
-}
-catch (exception $e) {
-    echo "Произошла ошибка - ",
-    $e->getMessage(),
+$combineNums = new CombineNums();
+//проверка на ошибки входных параметров
+if ($combineNums->getErrors($str, $len)) {
+    $combineNums->showErrors();
     die();
 }
-
-
-$combineNums = new CombineNums($str, $len);
+//Заполнение массива комбинаций подстроками
+$combineNums->combine($str, $len);
 var_dump($combineNums->getArray());
-echo $combineNums->getCountCombinations();
+//сверка с формулой
+echo "Количество размещений без повторений по формуле N!/(N-M)! = " .$combineNums->getCalculateCombinations($str, $len);
